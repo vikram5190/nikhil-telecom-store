39ce4b46dee1e77cade7ca498bfc89bcb27c7ef5
vikram5190 <vikram5190@gmail.com>
2026-02-18 16:53:58 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Nikhil newwww Telecom Store Live 🚀</h1>
-      <p>Your ecommerce system is being built p65465464roperly.</p>
+      <p>Your ecommerce syyutyustem is being built p65465464roperly.</p>
     </div>
   );
 }
