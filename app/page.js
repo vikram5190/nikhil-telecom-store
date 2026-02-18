d1bb5306a68290ce0dbd8ce48c02aa0d44e3f750
vikram5190 <vikram5190@gmail.com>
2026-02-18 15:31:14 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to Nikhil Telecom 🚀</h1>
+      <p>Your ecommerce system is being built properly.</p>
+    </div>
+  );
+}
