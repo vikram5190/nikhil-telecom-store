540ce68b28e48567ec67588ff0f1a87479f6f895
vikram5190 <vikram5190@gmail.com>
2026-02-18 15:30:53 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,15 @@
+export const metadata = {
+  title: "Nikhil Telecom",
+  description: "Online Mobile Accessories Store",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body style={{ fontFamily: "Arial, sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
 
