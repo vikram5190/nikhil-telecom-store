7392c7b2ae6c9a9a2966d0ff0b3bca058116fee5
vikram5190 <vikram5190@gmail.com>
2026-02-18 15:17:09 +0530

    Delete pages directory

--- pages/admin.js
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const AdminPage = dynamic(() => import("../components/AdminPage"), {
-  ssr: false,
-});
-
-export default function Admin() {
-  return <AdminPage />;
-}
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Welcome to Nikhil Telecom 🚀</h1>
-      <p>Your online mobile accessories store is being set up.</p>
-    </div>
-  );
-}
